b6104dbdd4187f0e3c315712dec8f06605caf2c9
Jon Vogel <joninsky@gmail.com>
2015-01-27 14:12:13 -0800

    More Twitter trend engine stuff

--- TwitterInvestor/TrendEngineForTicker.swift
+++ TwitterInvestor/TrendEngineForTicker.swift
@@ -30,7 +30,7 @@ class TrendEngineForTicker{
   //Do we have baseline trend data for this ticker? Or are we going to tell the user that there is no baseline data. We can expect that there will be no baseline data on stocks that are very obscure
   var needsBaseline:Bool = true
   //If we do have baseline data for the tweet then we want to display it as Tweets per hour to the user.
-  var tweetsPerHour: Int?
+  var tweetsPerHour: Double?
   //If we do have baseline data for this tweet, this data is the date of the oldest and newest tweets we have gotten.
   var idOfNewestTweet: String?
   //Date stamp of newest Tweet
@@ -50,12 +50,10 @@ class TrendEngineForTicker{
     self.ticker = tickerSymbol
     if firstJSONBlob.count == 0{
       tweetsPerHour = 0
-      println(self.arrayOfAllJSON.count)
     }else {
       for item in firstJSONBlob{
         self.arrayOfAllJSON.append(item)
       }
-      println(self.arrayOfAllJSON.count)
       let newestTweet = firstJSONBlob.first as [String:AnyObject]!
       self.idOfNewestTweet = newestTweet["id_str"] as? String
       let format = NSDateFormatter()
@@ -64,12 +62,21 @@ class TrendEngineForTicker{
       let oldestTweet = firstJSONBlob.last as [String:AnyObject]!
       self.idOfOldestTweet = oldestTweet["id_str"] as? String
       self.dateOfOldestTweet = format.dateFromString(oldestTweet["created_at"] as String!)
-      getMoreTweets(self.ticker!, oldestTweetID: self.idOfOldestTweet!)
+      getMoreTweets(self.ticker!, oldestTweetID: self.idOfOldestTweet!, completion: { (theBool) -> Void in
+        if theBool == true{
+          self.arrayOfAllJSON = self.stripTweets(self.arrayOfAllJSON)
+        }
+        println(self.arrayOfAllJSON.count)
+        self.tweetsPerHour = self.figureOutAverageInterval(self.arrayOfAllJSON)
+        println("averageTime Between Relevant Tweets is: \(self.tweetsPerHour!) minutes.")
+      })
+      self.needsBaseline = false
+    
     }
   }
   
   
-  func getMoreTweets(theTicker: String,oldestTweetID: String){
+  func getMoreTweets(theTicker: String,oldestTweetID: String, completion: (Bool)->Void){
     
     let fiveDaysAgo = NSDate(timeIntervalSinceNow: -430000)
     
@@ -79,7 +86,6 @@ class TrendEngineForTicker{
         for item in returnedJSON!{
           self.arrayOfAllJSON.append(item)
         }
-        println(self.arrayOfAllJSON.count)
         let oldestTweet = self.arrayOfAllJSON.last as [String:AnyObject]!
         self.idOfOldestTweet = oldestTweet["id_str"] as? String
         let format = NSDateFormatter()
@@ -87,10 +93,14 @@ class TrendEngineForTicker{
         self.dateOfOldestTweet = format.dateFromString(oldestTweet["created_at"] as String!)
         //completion(format.dateFromString(oldestTweet["created_at"] as String!)!)
         self.dateOfOldestTweet = format.dateFromString(oldestTweet["created_at"] as String!)
-        self.getMoreTweets(self.ticker!, oldestTweetID: self.idOfOldestTweet!)
-        println(self.dateOfOldestTweet)
+        self.getMoreTweets(self.ticker!, oldestTweetID: self.idOfOldestTweet!, completion: { (theBool) -> Void in
+          
+          completion(theBool)
+          
+        })
       }
     }else{
+      completion(true)
       return
     }
   }
@@ -104,7 +114,6 @@ class TrendEngineForTicker{
     dateFormat.dateFormat = "EEE MMM dd HH:mm:ss Z yyyy"
     for o in JSONBlob{
       let dateOfTweet = dateFormat.dateFromString(o["created_at"] as String)
-      //println(dateOfTweet)
       arrayOfDatesFromJSON.append(dateOfTweet!)
     }
     
@@ -120,15 +129,15 @@ class TrendEngineForTicker{
     for number in arrayOfTimeIntervals{
       totalIntervalTime = totalIntervalTime + number
     }
-    println(arrayOfDatesFromJSON.count)
-    return (totalIntervalTime/Double(arrayOfTimeIntervals.count))
+    return (totalIntervalTime/Double(arrayOfTimeIntervals.count))/60
   }
   
   
   //Funciton to strip tweets that have nothing to do with investing.
   private func stripTweets(JSONBlob: [[String:AnyObject]])->[[String:AnyObject]]{
     var JSON = JSONBlob
-    let arrayOfKeyWords = ["Stock","Market","Money","Mover","investing","DayTrader", "loser", "Gainer", "PreMarket", "Soared", "rating", "buy", "sell", "stock", "chart", "longterm", "Trade","investment", "long", "short"]
+    //make sure all lower case
+    let arrayOfKeyWords = ["stock","market","money","mover","investing","daytrader", "loser", "gainer", "premarket", "soared", "rating", "buy", "sell", "stock", "chart", "longterm", "trade","investment", "long", "short"]
     var investmentRelatedTweets = [[String:AnyObject]]()
     
     for var i = 0; i < JSON.count; ++i {
@@ -140,11 +149,15 @@ class TrendEngineForTicker{
       for o in hashTags{
         arrayOfHashTags.append(o["text"] as String!)
       }
+      //revisit this logic
       for k in arrayOfKeyWords{
         if text.lowercaseString.rangeOfString(k) != nil {
-            investmentRelatedTweets.append(JSON[i])
+          for HT in arrayOfHashTags{
+            if HT.lowercaseString.rangeOfString(HT) != nil{
+              investmentRelatedTweets.append(JSON[i])
+            }
+          }
         }
-        
       }
     }
     return investmentRelatedTweets
