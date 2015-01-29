d9468d7fafce757749b80ce386924a4a75750f3d
Jon Vogel <joninsky@gmail.com>
2015-01-29 09:23:01 -0800

    Morning SYNC

--- TwitterInvestor/NetworkController.swift
+++ TwitterInvestor/NetworkController.swift
@@ -90,57 +90,6 @@ class NetworkController {
     }
   
   
-
-//////  func getInitialTwitterRequest(tickerSymbol: String, trailingClosure: (TrendEngineForTicker?,NSError?)->Void) {
-//////    let myAccountStore = ACAccountStore()
-//////    //Create a variable of type ACAccountType by using the method accountTypeWithAccountTypeIdentifier thats in ACAccountStore
-//////    let myAccountType = myAccountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
-//////    //this starts a new thread to access the account and do something with it in the clsure statement
-//////    myAccountStore.requestAccessToAccountsWithType(myAccountType, options: nil) { (gotit: Bool , error: NSError!) -> Void in
-//////      //If ACAccountStore was able to access the account the gotit boolean in the cluses will be true or false
-//////      if gotit{
-//////        //A user can have multiple accounts with each of these services, we load them all into the array.
-//////        let accountsArray = myAccountStore.accountsWithAccountType(myAccountType)
-//////        //make sure we got at least one account
-//////        if accountsArray.isEmpty == false{
-//////          //We just want the first account that is stored in the array of account (there might only be one)
-//////          self.twitterAccount = accountsArray[0] as? ACAccount
-//////          //return twitterAccount
-//////          let requestURL = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json?q=%23\(tickerSymbol)&count=100")//q=%SIRI")
-//////          //A request of type SLRequest, this starts a new thread.
-//////          let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
-//////          //Set the SLRequests account property to the twitter accont that we got from the array of accounts
-//////          twitterRequest.account = self.twitterAccount
-//////          //call the performRequestWithHandler method on SLRequest (this starts a new thread) paramaters are the data that will be returned, the response code, and then an error
-//////          twitterRequest.performRequestWithHandler({ (jsonData, responseCode, error) -> Void in
-//////            println(error)
-//////            //make a switch statement on the response code. You will probably get a basic server response code
-//////            switch responseCode.statusCode {
-//////              //If the response is good
-//////            case 200...299:
-//////              //create an array of json data that is typed as an array of [AnyObject]
-//////              //println(jsonData)
-//////              if let jsonDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: nil) as? [String: AnyObject] {
-//////                if let arrayOfResults = jsonDictionary["statuses"] as? [[String:AnyObject]]{
-//////                  NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-//////                    trailingClosure(TrendEngineForTicker(tickerSymbol: tickerSymbol,firstJSONBlob: arrayOfResults),nil)
-//////                  })
-//////                }
-//////              }
-//////              //If response is bad
-//////            case 400...599:
-//////              println(responseCode.statusCode)
-//////              NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-//////                trailingClosure(nil, error)
-//////              })
-//////            default:
-//////              println("\(responseCode.statusCode)")
-//////            }
-//////          })
-//////        }
-//////      }
-//////    }
-//////  }
   func twitterRequestForSinceID(tickerSymbol: String, theID: String, completion: ([[String:AnyObject]]?,NSError?)->Void){
     let myAccountStore = ACAccountStore()
     //Create a variable of type ACAccountType by using the method accountTypeWithAccountTypeIdentifier thats in ACAccountStore
@@ -156,10 +105,6 @@ class NetworkController {
           //We just want the first account that is stored in the array of account (there might only be one)
           self.twitterAccount = accountsArray[0] as? ACAccount
           //return twitterAccount
-          
-          //This is the URL for the JSON twitter request for hometimeline
-          //https://stream.twitter.com/1.1/statuses/firehose.json
-          //https://api.twitter.com/1.1/search/tweets.json?q=%23yolo&count=200&src=typd
           let requestURL = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json?q=%23\(tickerSymbol)&count=100&max_id=\(theID)")
           //A request of type SLRequest, this starts a new thread.
           let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
@@ -249,6 +194,60 @@ class NetworkController {
     }
   }
   
+//  func overloadTwitter(tickerSymbol: String, trailingClosure: (TrendEngineForTicker?,NSError?)->Void){
+//    let myAccountStore = ACAccountStore()
+//    //Create a variable of type ACAccountType by using the method accountTypeWithAccountTypeIdentifier thats in ACAccountStore
+//    let myAccountType = myAccountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+//    //this starts a new thread to access the account and do something with it in the clsure statement
+//    myAccountStore.requestAccessToAccountsWithType(myAccountType, options: nil) { (gotit: Bool , error: NSError!) -> Void in
+//      if gotit{
+//        //A user can have multiple accounts with each of these services, we load them all into the array.
+//        let accountsArray = myAccountStore.accountsWithAccountType(myAccountType)
+//        //make sure we got at least one account
+//        if accountsArray.isEmpty == false{
+//          //We just want the first account that is stored in the array of account (there might only be one)
+//          self.twitterAccount = accountsArray[0] as? ACAccount
+//          //return twitterAccount
+//          let requestURL = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json?q=%23\(tickerSymbol)&count=100")//q=%SIRI")
+//          //A request of type SLRequest, this starts a new thread.
+//          //var trendEngine: TrendEngineForTicker?
+//          let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
+//          //Set the SLRequests account property to the twitter accont that we got from the array of accounts
+//          twitterRequest.account = self.twitterAccount
+//          //call the performRequestWithHandler method on SLRequest (this starts a new thread) paramaters are the data that will be returned, the response code, and then an error
+//          twitterRequest.performRequestWithHandler({ (jsonData, responseCode, error) -> Void in
+//            println(error)
+//            //make a switch statement on the response code. You will probably get a basic server response code
+//            switch responseCode.statusCode {
+//              //If the response is good
+//            case 200...299:
+//              //create an array of json data that is typed as an array of [AnyObject]
+//              //println(jsonData)
+//              if let jsonDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: nil) as? [String: AnyObject] {
+//                if let arrayOfResults = jsonDictionary["statuses"] as? [[String:AnyObject]]{
+//                 //trendEngine = TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults)
+//                  trailingClosure(TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults), nil)
+//                }
+//              }
+//              //If response is bad
+//            case 400...599:
+//              //println(responseCode.statusCode)
+//              NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+//                trailingClosure(nil, error)
+//              })
+//            default:
+//              println("\(responseCode.statusCode)")
+//            }
+//          })
+//        //end accounts array is empty check
+//        }
+//      //got it check
+//      }
+//    //end account store request
+//    }
+//   //end function
+//  }
+  
   func overloadTwitter(tickerSymbol: String, trailingClosure: (TrendEngineForTicker?,NSError?)->Void){
     let myAccountStore = ACAccountStore()
     //Create a variable of type ACAccountType by using the method accountTypeWithAccountTypeIdentifier thats in ACAccountStore
@@ -262,7 +261,9 @@ class NetworkController {
         if accountsArray.isEmpty == false{
           //We just want the first account that is stored in the array of account (there might only be one)
           self.twitterAccount = accountsArray[0] as? ACAccount
+          var trendEngineToReturn: TrendEngineForTicker?
           //return twitterAccount
+         // if trendEngineToReturn == nil{
           let requestURL = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json?q=%23\(tickerSymbol)&count=100")//q=%SIRI")
           //A request of type SLRequest, this starts a new thread.
           //var trendEngine: TrendEngineForTicker?
@@ -280,8 +281,8 @@ class NetworkController {
               //println(jsonData)
               if let jsonDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: nil) as? [String: AnyObject] {
                 if let arrayOfResults = jsonDictionary["statuses"] as? [[String:AnyObject]]{
-                 //trendEngine = TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults)
-                  trailingClosure(TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults), nil)
+                  //trendEngine = TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults)
+                  trendEngineToReturn = TrendEngineForTicker(tickerSymbol: tickerSymbol, firstJSONBlob: arrayOfResults)
                 }
               }
               //If response is bad
@@ -294,16 +295,24 @@ class NetworkController {
               println("\(responseCode.statusCode)")
             }
           })
-        //end accounts array is empty check
+          //end If to check for initial JSON request
+         // }else{
+            
+            
+            
+            
+            
+          //End else to build all the data and call the completion handler.
+         // }
+          //end accounts array is empty check
         }
-      //got it check
+        //got it check
       }
-    //end account store request
+      //end account store request
     }
-   //end function
+    //end function
   }
-  
-  
+
   
   
     
